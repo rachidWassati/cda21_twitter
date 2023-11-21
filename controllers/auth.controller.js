042cb2b291f814d4af2c90450da3de56e35ab120
rachidWassati <rachid.jeffali@wassati.com>
2023-11-21 17:32:13 +0100

    Mise en place de l'authentification

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,18 +1,30 @@
+const passport = require("passport")
+
 
 exports.signinForm = async (req, res, next) => {
     try {
-        res.end()
+        res.render('auth/signin')
     } catch (error) {
         next(error)
     }
 }
 
-exports.signin = async (req, res, next) => {
-    try {
-        res.end()
-    } catch (error) {
-        next(error)
-    }
+exports.signin = (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if(err) {
+            next(err)
+        } else if(!user) {
+            res.render('auth/signin', {errors: [info.message]})
+        } else {
+            req.login(user, err => {
+                if(err) {
+                    next(err)
+                } else {
+                    res.redirect('/')
+                }
+            })
+        }
+    })(req, res, next)
 }
 
 exports.signout = async (req, res, next) => {
